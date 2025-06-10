b1cbb72fabc99ac1f5c9d8b91679977ce985b0e6
DeveloperAmrit <105799602+DeveloperAmrit@users.noreply.github.com>
2025-06-10 16:42:02 +0530

    Completed the navigation side bar for servers 	modified:   app/(main)/layout.tsx 	modified:   app/layout.tsx 	new file:   components/modals/create-server-modal.tsx 	modified:   components/mode-toggle.tsx 	modified:   components/navigation/navigation-action.tsx 	new file:   components/providers/modal-provider.tsx 	new file:   hooks/use-modal-store.ts 	modified:   package-lock.json 	modified:   package.json

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -18,9 +18,9 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+        <Button className="!bg-transparent hover:!bg-zinc-300 dark:hover:!bg-zinc-800 !border-0 !ring-0" variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 transition-all dark:-rotate-90 !scale-100 dark:!scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 transition-all dark:rotate-0 !scale-0 dark:!scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
